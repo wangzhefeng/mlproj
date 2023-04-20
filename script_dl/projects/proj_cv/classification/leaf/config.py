f0ed57a0528932e56bccb7d7b32acda21f3392a7
wangzhefeng <wangzhefengr@163.com>
2023-04-20 21:26:07 +0800

    update

--- script_dl/projects/proj_cv/classification/leaf/config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-
-
-root_dir = "/Users/zfwang/machinelearning"
-project_dir = os.path.join(root_dir, "deeplearning/src/project_image/image_classification/leaf_classification")
-data_dir = os.path.join(root_dir, "datasets/leaf_classification")
